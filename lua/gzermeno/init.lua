02215c94b54bb76b49ac80de9be169e9f8e86b49
Gabriel Zermeno <gzermeno1@gmail.com>
2024-04-13 15:45:52 -0700

    initial commit

---
+++ lua/gzermeno/init.lua
@@ -0,0 +1,16 @@
+require("gzermeno.lazy_init")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.opt.termguicolors = true
+
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
